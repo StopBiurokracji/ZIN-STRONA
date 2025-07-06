527502164af880a719e19d6eef2ab90ff1fe5f10
StopBiurokracji <robert.kochaniak@wp.pl>
2025-07-06 13:55:15 +0200

    Add files via upload

--- counter.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-  const podpisy = document.getElementById("liczba-podpisow");
-  const odwiedziny = document.getElementById("liczba-odwiedzin");
-  if (podpisy) podpisy.textContent = "1423";
-  if (odwiedziny) odwiedziny.textContent = "5189";
-});
